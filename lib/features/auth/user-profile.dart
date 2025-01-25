2ae5432aa7ba48afc53c7b4012e5ef902f15f0bb
Hanan-Ashraf-Mousa <mosah2312@gmail.com>
2025-01-25 18:20:27 +0200

    add profile bloc

---
+++ lib/features/auth/user-profile.dart
@@ -0,0 +1,8 @@
+class UserProfile{
+  String? path;
+  String name ;
+  String email ;
+  String password ;
+  String phone ;
+  UserProfile({required this.name, required this.email, required this.password,required this. phone, this.path});
+}
